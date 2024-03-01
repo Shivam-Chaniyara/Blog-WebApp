e6bbcc04957be47e73dddde15a389caf75d786df
Shivam-Chaniyara <shivamchaniyara41@gmail.com>
2024-03-01 14:27:14 +0530

    add Folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { useDispatch } from 'react-redux'
 import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
+import Header from  './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -22,7 +24,15 @@ function App() {
   }, [])
 
   return !loading ? (
-    <div className='h'></div>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        {/* <main> */}
+          {/* <Outlet /> */}
+        {/* </main> */}
+        <Footer />
+      </div>
+    </div>
   ) : (null)
 }
 
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import appWriteService from '../appwrite/config'
+import { Link } from 'react-router-dom'
+
+function PostCard() {
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
